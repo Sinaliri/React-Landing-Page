51510a02d6c7211fa3f23533b28a40c0cfec9574
SinaLiri <Sina.Liri@yahoo.com>
2022-08-07 13:48:33 +0430

    searchbar onchane method add

--- landing-page/src/component/SearchBar/SearchBar.jsx
+++ landing-page/src/component/SearchBar/SearchBar.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
 import styles from "./SearchBar.module.css"
 export default class SearchBar extends Component {
+  constructor(){
+    super();
+    this.state=({
+      text:""
+    })
+  }
+  ChangeHandler=(event)=>{
+    this.setState({
+      text:event.target.value
+    })
+  }
   render() {
+    const {text}=this.state
     return (
       <div className={styles.container}>
         <p>Search every thing you want</p>
-        <input placeholder='Search...'/>
+       <div className={styles.inputcontainer}>
+       <input value={text} onChange={this.ChangeHandler} placeholder='Search...'/>
+        <span>{text}</span>
+       </div>
       </div>
     )
   }
